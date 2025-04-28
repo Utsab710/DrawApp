32323f9b38ad11bf211c1ae80d36560fa3e423b6
Your Name <shresthautsab789@gmail.com>
2025-04-28 15:37:21 +0545

    Adding the db

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,63 @@
+import express from "express";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import jwt from "jsonwebtoken";
+import { middleware } from "./middleware";
+import {
+  CreateUserSchema,
+  SigninSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
+
+const app = express();
+
+app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+  //dbcall
+  res.json({
+    userId: "123",
+  });
+});
+
+app.post("/signin", (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
+  res.json({
+    token,
+  });
+});
+
+app.post("/room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+  //db call
+
+  res.json({
+    userId: "123",
+  });
+});
+
+app.listen(3001);
